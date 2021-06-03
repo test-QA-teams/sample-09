838adffa366b0658d2e0fc2b70bbde7b634e9563
Purusothaman <purushothaman@opsera.io>
2021-06-03 14:41:56 +0530

    sample spring boot example

---
+++ src/main/java/com/opsera/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.opsera.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello!, Welcome to Opsera!";
+    }
+}
